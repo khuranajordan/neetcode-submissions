feda0c059ee3c59b268aec30a76a80f6e4b33e0b
Rohan Khurana <32005723+khuranajordan@users.noreply.github.com>
2026-05-21 22:27:24 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    s = []
+    encode(strs) {
+        this.s = strs
+        return "gebberish"
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+
+        return this.s
+    }
+}
